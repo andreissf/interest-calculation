5fc0743e6f79c72c43c6a1bcc3d211870be59616
Andrei Silva da Silva <andreissf@gmail.com>
2019-03-20 22:50:09 -0300

    xunit teste validao servico

--- InterestCalc/Softplan.Test.Services/InterestService.cs
+++ InterestCalc/Softplan.Test.Services/InterestService.cs
@@ -1,18 +1,22 @@
-﻿using Softplan.Test.Interface.Service;
+﻿using Microsoft.Extensions.Configuration;
+using Softplan.Test.Interface.Service;
 using System;
 
 namespace Softplan.Test.Services
 {
     public class InterestService : IInterestService
     {
+        private readonly double FIXED_INTEREST = 0.001;
+
         public InterestService()
         {
-
+            
         }
 
         public decimal Calc(decimal value, int months)
         {
-            return value;
+            var vf = (value + (value * Convert.ToDecimal(FIXED_INTEREST) + months)).ToString("###.##");
+            return Convert.ToDecimal(String.Format("{0:0.00}", Convert.ToDecimal(vf)));
         }
     }
 }
